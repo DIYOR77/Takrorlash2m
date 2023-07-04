30e62aac60509bdad654a043a8a3fb7d104a1a9c
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-07-04 17:58:47 +0500

    takrorlash

---
+++ takrorlash/takrorlash/Duck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace takrorlash
+{
+    internal class Duck : Animal,FLY,SWIM
+    {
+        public Duck(string name,int age)
+            : base(name, age)
+        {
+
+        }
+        public override void Eat()
+        {
+            Console.WriteLine($"{Name} is eating.");
+        }
+        public override void Sleep()
+        {
+            Console.WriteLine($"{Name} is sleeping.");
+        }
+        public void swim()
+        {
+            Console.WriteLine($"{Name} is swiming.");
+        }
+        public void fly()
+        {
+            Console.WriteLine($"{Name} is flying.");
+        }
+    }
+}
--- takrorlash/takrorlash/Program.cs
+++ takrorlash/takrorlash/Program.cs
@@ -4,7 +4,92 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            START();
+            Main(args);
+        }
+        public static void START()
+        {
+            Brid brid = new Brid("LAYLAK", 3);
+            Fish fish = new Fish("SAZAN", 1);
+            Duck duck = new Duck("OQ", 2);
+            Console.WriteLine(" 1 => Brid : 2 => Fish : 3 => Duck ");
+            Console.Write(" Choose an animal: ");
+            int animal=int.Parse(Console.ReadLine());
+            switch (animal) 
+            {
+                case 1:
+                    {
+                        Console.WriteLine(" 1 => Eat : 2 => Sleep : 3 => Fly ");
+                        Console.Write(" Sesect operation: ");
+                        int operation=int.Parse(Console.ReadLine());
+                        switch(operation)
+                        {
+                            case 1:
+                                brid.Eat();
+                                break;
+                            case 2:
+                                brid.Sleep();
+                                break;
+                            case 3:
+                                brid.fly();
+                                break;
+                            default:
+                                 START();
+                                break;
+                        }
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine(" 1 => Eat : 2 => Sleep : 3 => Swim ");
+                        Console.Write(" Sesect operation: ");
+                        int operation = int.Parse(Console.ReadLine());
+                        switch (operation)
+                        {
+                            case 1:
+                                fish.Eat();
+                                break;
+                            case 2:
+                                fish.Sleep();
+                                break;
+                            case 3:
+                                fish.swim();
+                                break;
+                            default:
+                                START();
+                                break;
+                        }
+                    }
+                    break;
+                case 3: 
+                    {
+                        Console.WriteLine(" 1 => Eat : 2 => Sleep : 3 => Swim : 4 => Fly");
+                        Console.Write(" Sesect operation: ");
+                        int operation = int.Parse(Console.ReadLine());
+                        switch (operation)
+                        {
+                            case 1:
+                                duck.Eat();
+                                break;
+                            case 2:
+                                duck.Sleep();
+                                break;
+                            case 3:
+                                duck.swim();
+                                break;
+                            case 4:
+                                duck.fly();
+                                break;
+                            default:
+                                START();
+                                break;
+                        }
+                    }
+                    break;
+                default:
+                    START();
+                    break;
+            }
         }
     }
 }
